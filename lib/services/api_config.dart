c1b08d6ba6b6140d5f48b749dbb3f70d24d9cab7
Tatiana-Ruco05 <Sena.tatiana05@gmail.com>
2026-06-23 05:42:28 -0500

    hola

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -17,7 +17,7 @@ class ApiConfig {
 
   static const String baseUrl = String.fromEnvironment(
     'SIMA_API_URL',
-    defaultValue: 'http://192.168.80.220:3000/api',
+    defaultValue: 'http://192.168.101.3:3000/api',
   );
 
   // Endpoints de autenticación
